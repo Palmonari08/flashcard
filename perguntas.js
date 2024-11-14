cf98c24aba5dddb59f08d035fc1a54313bab9f3c
Palmonari08 <isaquepalmonari394@gmail.com>
2024-11-13 21:12:12 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que é adição?',
+    'é a soma de dois ou mais números'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital da argentina?',
+    'A capital da argentina é buenos aires'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é uma palavra?',
+    'uma palavra é a formação de letras que formam sílabas'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz tchau em Inglês?',
+    'bye em ingles é bye'
 )
